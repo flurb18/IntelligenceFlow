d1d8f47d9f92dec4d01e4dacf1f0cfc68dc0c6a2
Flurb <flurb18@gmail.com>
2023-09-11 11:50:31 -0400

    expand

--- web/src/app.js
+++ web/src/app.js
@@ -2,12 +2,12 @@ document.addEventListener('DOMContentLoaded', function() {
   const cy = cytoscape({
     container: document.getElementById('flow-diagram'),
     zoom: 1,
-    minZoom: 0.2,
-    maxZoom: 5,
+    minZoom: 0.5,
+    maxZoom: 2,
     elements: [
       { data: { id: 'Input' } },
       { data: { id: 'Output' } },
-      { data: { id: 'ab', source: 'a', target: 'b' } }
+      { data: { id: 'ab', source: 'Input', target: 'Output' } }
     ],
     style: [
       {
@@ -82,25 +82,27 @@ const blockTypes = {
     }
 }
 
+// Create block type submenus
 for (var blockType of Object.keys(blockTypes)) {
     // Create option in dropdown
     var blockTypeOption = document.createElement("option");
     blockTypeOption.setAttribute("value", blockType);
     blockTypeOption.innerText = blockType;
     document.getElementById("new-block-type").appendChild(blockTypeOption);
-    // Create form for type
-    var blockTypeForm = document.createElement("form");
+    // Create submenu for type
+    var blockTypeForm = document.createElement("div");
     blockTypeForm.setAttribute("id", "new-block-" + blockType + "-submenu");
     blockTypeForm.setAttribute("name", blockType);
     blockTypeForm.setAttribute("class", "new-block-type-submenu");
     let params = blockTypes[blockType]["parameters"];
     for (var param of Object.keys(params)) {
+        var paramLabel = document.createElement("label");
+        paramLabel.setAttribute("for", param);
+        paramLabel.innerText = params[param]["label"];
+        blockTypeForm.appendChild(paramLabel);
         switch (params[param]["type"]) {
             //Dropdown menu
             case "choice":
-                var paramLabel = document.createElement("label");
-                paramLabel.setAttribute("for", param);
-                paramLabel.innerText = params[param]["label"];
                 var paramChoice = document.createElement("select");
                 paramChoice.setAttribute("id", param);
                 paramChoice.setAttribute("name", param);
@@ -110,53 +112,69 @@ for (var blockType of Object.keys(blockTypes)) {
                     paramChoiceOption.innerText = choice;
                     paramChoice.appendChild(paramChoiceOption);
                 }
-                blockTypeForm.appendChild(paramLabel);
                 blockTypeForm.appendChild(paramChoice);
+                blockTypeForm.appendChild(document.createElement("br"));
                 break;
             //Textbox
             case "text":
-                var paramLabel = document.createElement("label");
-                paramLabel.setAttribute("for", param);
-                paramLabel.innerText = params[param]["label"];
                 var paramText = document.createElement("input");
                 paramText.setAttribute("id", param);
                 paramText.setAttribute("name", param);
                 paramText.setAttribute("type", "text");
-                blockTypeForm.appendChild(paramLabel);
                 blockTypeForm.appendChild(paramText);
+                blockTypeForm.appendChild(document.createElement("br"))
                 break;
             //Numeric input
             case "num":
-                var paramLabel = document.createElement("label");
-                paramLabel.setAttribute("for", param);
-                paramLabel.innerText = params[param]["label"];
                 var paramNum = document.createElement("input");
                 paramNum.setAttribute("id", param);
                 paramNum.setAttribute("name", param);
                 paramNum.setAttribute("type", "range");
                 paramNum.setAttribute("min", params[param]["min"]);
                 paramNum.setAttribute("max", params[param]["max"]);
-                blockTypeForm.appendChild(paramLabel);
                 blockTypeForm.appendChild(paramNum);
+                blockTypeForm.appendChild(document.createElement("br"))
                 break;
+            //File input
             case "file":
+                var paramFile = document.createElement("input");
+                paramFile.setAttribute("id", param);
+                paramFile.setAttribute("name", param);
+                paramFile.setAttribute("type", "file");
+                blockTypeForm.appendChild(paramFile);
+                blockTypeForm.appendChild(document.createElement("br"));
                 break;
             default:
                 break;
         }
     }
-    document.getElementById("new-block-form").appendChild(blockTypeForm);
+    document.getElementById("new-block-type-params").appendChild(blockTypeForm);
 }
 
+// Hide submenus based on type choice
 var newBlockTypePicker = document.getElementById("new-block-type");
 newBlockTypePicker.addEventListener("change", function () {
     let selectedValue = newBlockTypePicker.options[newBlockTypePicker.selectedIndex].value;
     let subForms = document.getElementsByClassName("new-block-type-submenu");
     for (let i = 0; i < subForms.length; i += 1) {
         if (selectedValue === subForms[i].name) {
-          subForms[i].setAttribute('style', 'display:block');
+          subForms[i].style.display = "block";
         } else {
-          subForms[i].setAttribute('style', 'display:none');
+          subForms[i].style.display = "none";
         }
     }
 });
+
+// Make sidebar expand buttons work
+var expands = document.getElementsByClassName("sidebar-submenu-expand");
+for (var i = 0; i < expands.length; i++) {
+  expands[i].addEventListener("click", function() {
+    this.classList.toggle("sidebar-submenu-expand-active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
