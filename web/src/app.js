b5107bac354da549a530c0b7ffb8c7934681b2c6
Flurb <flurb18@gmail.com>
2023-09-11 19:17:11 -0400

    multiple changes

--- web/src/app.js
+++ web/src/app.js
@@ -82,95 +82,134 @@ const blockTypes = {
     }
 }
 
-// Create block type submenus
-for (var blockType of Object.keys(blockTypes)) {
-    // Create option in dropdown
-    var blockTypeOption = document.createElement("option");
-    blockTypeOption.setAttribute("value", blockType);
-    blockTypeOption.innerText = blockType;
-    document.getElementById("new-block-type").appendChild(blockTypeOption);
-    // Create submenu for type
-    var blockTypeForm = document.createElement("div");
-    blockTypeForm.setAttribute("id", "new-block-" + blockType + "-submenu");
-    blockTypeForm.setAttribute("name", blockType);
-    blockTypeForm.setAttribute("class", "new-block-type-submenu");
-    let params = blockTypes[blockType]["parameters"];
-    for (var param of Object.keys(params)) {
-        var paramLabel = document.createElement("label");
-        paramLabel.setAttribute("for", param);
-        paramLabel.innerText = params[param]["label"];
-        blockTypeForm.appendChild(paramLabel);
-        switch (params[param]["type"]) {
-            //Dropdown menu
-            case "choice":
-                var paramChoice = document.createElement("select");
-                paramChoice.setAttribute("id", param);
-                paramChoice.setAttribute("name", param);
-                for (var choice of params[param]["choices"]) {
-                    var paramChoiceOption = document.createElement("option");
-                    paramChoiceOption.setAttribute("value", choice);
-                    paramChoiceOption.innerText = choice;
-                    paramChoice.appendChild(paramChoiceOption);
-                }
-                blockTypeForm.appendChild(paramChoice);
-                blockTypeForm.appendChild(document.createElement("br"));
-                break;
-            //Textbox
-            case "text":
-                var paramText = document.createElement("input");
-                paramText.setAttribute("id", param);
-                paramText.setAttribute("name", param);
-                paramText.setAttribute("type", "text");
-                blockTypeForm.appendChild(paramText);
-                blockTypeForm.appendChild(document.createElement("br"))
-                break;
-            //Numeric input
-            case "num":
-                var paramNum = document.createElement("input");
-                paramNum.setAttribute("id", param);
-                paramNum.setAttribute("name", param);
-                paramNum.setAttribute("type", "range");
-                paramNum.setAttribute("min", params[param]["min"]);
-                paramNum.setAttribute("max", params[param]["max"]);
-                blockTypeForm.appendChild(paramNum);
-                blockTypeForm.appendChild(document.createElement("br"))
-                break;
-            //File input
-            case "file":
-                var paramFile = document.createElement("input");
-                paramFile.setAttribute("id", param);
-                paramFile.setAttribute("name", param);
-                paramFile.setAttribute("type", "file");
-                blockTypeForm.appendChild(paramFile);
-                blockTypeForm.appendChild(document.createElement("br"));
-                break;
-            default:
-                break;
+const apiTypes = {
+    "Oobabooga" : {
+        "parameters" : {
+
+        }
+    },
+    "OpenAI" : {
+        "parameters" : {
+            "OpenAI-APIkey" : {
+                "label" : "API Key",
+                "type" : "text"
+            }
         }
     }
-    document.getElementById("new-block-type-params").appendChild(blockTypeForm);
 }
 
-// Hide submenus based on type choice
-var newBlockTypePicker = document.getElementById("new-block-type");
-newBlockTypePicker.addEventListener("change", function () {
-    let selectedValue = newBlockTypePicker.options[newBlockTypePicker.selectedIndex].value;
-    let subForms = document.getElementsByClassName("new-block-type-submenu");
-    for (let i = 0; i < subForms.length; i += 1) {
-        if (selectedValue === subForms[i].getAttribute("name")) {
-            subForms[i].style.display = "block";
-        } else {
-            subForms[i].style.display = "none";
-        }
+// Append a immediately after e
+function insertAfter(a, e) {
+    if (e.nextSibling) {
+        e.parentNode.insertBefore(a, e.nextSibling);
+    } else {
+        e.parentNode.appendChild(a);
     }
-});
+}
+
+// Creates submenus that are only visisble according to the choice of selectElement, according to the config object.
+// config should consist of string:object pairs, where the string is an option to be added to selectElement, and the object
+// describes the submenu to be created. The object should have an entry "parameters" mapping to an object of string:object pairs,
+// where this object has an entry "label" and an entry "type". "type" should be in choice,text,num,file. "label" is what goes next to
+// the field in the submenu.
+function createSubmenusByType (config, selectElement) {
+    for (var type of Object.keys(config)) {
+        // Create option in selectElement
+        var typeOption = document.createElement("option");
+        typeOption.setAttribute("value", type);
+        typeOption.innerText = type;
+        selectElement.appendChild(typeOption);
+        // Create submenu
+        var typeSubmenu = document.createElement("div");
+        var typeSubmenuHeader = document.createElement("h1");
+        typeSubmenuHeader.innerText = type + " Parameters";
+        typeSubmenu.appendChild(typeSubmenuHeader);
+        typeSubmenu.appendChild(document.createElement("br"));
+        typeSubmenu.setAttribute("id", type +"-submenu");
+        typeSubmenu.setAttribute("name", type);
+        typeSubmenu.setAttribute("class", selectElement.getAttribute("name")+"-submenu")
+        typeSubmenu.style.display = "none";
+        let params = config[type]["parameters"];
+        for (var param of Object.keys(params)) {
+            var paramLabel = document.createElement("label");
+            paramLabel.setAttribute("for", param);
+            paramLabel.innerText = params[param]["label"];
+            typeSubmenu.appendChild(paramLabel);
+            switch (params[param]["type"]) {
+                //Dropdown menu
+                case "choice":
+                    var paramChoice = document.createElement("select");
+                    paramChoice.setAttribute("id", param);
+                    paramChoice.setAttribute("name", param);
+                    for (var choice of params[param]["choices"]) {
+                        var paramChoiceOption = document.createElement("option");
+                        paramChoiceOption.setAttribute("value", choice);
+                        paramChoiceOption.innerText = choice;
+                        paramChoice.appendChild(paramChoiceOption);
+                    }
+                    typeSubmenu.appendChild(paramChoice);
+                    typeSubmenu.appendChild(document.createElement("br"));
+                    break;
+                //Textbox
+                case "text":
+                    var paramText = document.createElement("input");
+                    paramText.setAttribute("id", param);
+                    paramText.setAttribute("name", param);
+                    paramText.setAttribute("type", "text");
+                    typeSubmenu.appendChild(paramText);
+                    typeSubmenu.appendChild(document.createElement("br"))
+                    break;
+                //Numeric input
+                case "num":
+                    var paramNum = document.createElement("input");
+                    paramNum.setAttribute("id", param);
+                    paramNum.setAttribute("name", param);
+                    paramNum.setAttribute("type", "range");
+                    paramNum.setAttribute("min", params[param]["min"]);
+                    paramNum.setAttribute("max", params[param]["max"]);
+                    typeSubmenu.appendChild(paramNum);
+                    typeSubmenu.appendChild(document.createElement("br"))
+                    break;
+                //File input
+                case "file":
+                    var paramFile = document.createElement("input");
+                    paramFile.setAttribute("id", param);
+                    paramFile.setAttribute("name", param);
+                    paramFile.setAttribute("type", "file");
+                    typeSubmenu.appendChild(paramFile);
+                    typeSubmenu.appendChild(document.createElement("br"));
+                    break;
+                default:
+                    break;
+            }
+        }
+        insertAfter(typeSubmenu, selectElement);
+        insertAfter(document.createElement("br"), selectElement);
+    }
+    selectElement.addEventListener("change", function () {
+        let selectedValue = selectElement.options[selectElement.selectedIndex].value;
+        let submenus = document.getElementsByClassName(selectElement.getAttribute("name")+"-submenu")
+        for (let i = 0; i < submenus.length; i += 1) {
+            if (selectedValue === submenus[i].getAttribute("name")) {
+                submenus[i].style.display = "block";
+            } else {
+                submenus[i].style.display = "none";
+            }
+        }
+    });
+}
+
+
+// Create block type submenus
+createSubmenusByType(blockTypes, document.getElementById("new-block-type"));
+createSubmenusByType(apiTypes, document.getElementById("api-settings-type"));
 
 // Make sidebar expand buttons work
-var expands = document.getElementsByClassName("sidebar-submenu-expand");
+var expands = document.getElementsByClassName("sidebar-submenu-expand-button");
 for (var i = 0; i < expands.length; i++) {
     expands[i].addEventListener("click", function (e) {
         e.preventDefault();
-        this.classList.toggle("sidebar-submenu-expand-active");
+        this.classList.toggle("sidebar-submenu-expand-button-active");
         var content = this.nextElementSibling;
         if (content.style.display === "block") {
             content.style.display = "none";
@@ -179,3 +218,9 @@ for (var i = 0; i < expands.length; i++) {
         }
     });
 }
+
+// Handle new block form submission
+var newBlockForm = document.getElementById("new-block-form");
+newBlockForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+});
