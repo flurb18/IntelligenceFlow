4c46da241450fa48447b0382cf5dff9eb6fd2bf1
Flurb <flurb18@gmail.com>
2023-09-12 17:48:53 -0400

    importing and fetching

--- web/src/app.js
+++ web/src/app.js
@@ -1,3 +1,147 @@
+import './utils.js';
+
+
+const blockTypes_backup = {
+    "INPUT": {
+        "maps": {
+            "none": ["single"]
+        },
+        "parameters": {}
+    },
+    "INPUT-FIXED": {
+        "maps": {
+            "none": ["single"]
+        },
+        "parameters": {
+            "INPUT-FIXED-text": {
+                "label": "Fixed Text Input",
+                "type": "textbox"
+            }
+        }
+    },
+    "OUTPUT": {
+        "maps": {
+            "none": ["none"],
+            "single": ["none"],
+            "multi": ["none"]
+        },
+        "parameters": {}
+    },
+    "COPY": {
+        "maps": {
+            "none": ["none"],
+            "single": ["multi"],
+            "multi": ["multi"]
+        },
+        "parameters": {
+            "COPY-num-copies": {
+                "label": "Number of Copies",
+                "type": "num",
+                "min": "2",
+                "max": "100"
+            }
+        }
+    },
+    "SPLIT": {
+        "maps": {
+            "none": ["none"],
+            "single": ["multi"],
+            "multi": ["multi"]
+        },
+        "parameters": {
+            "SPLIT-chunk-size": {
+                "label": "Chunk Size",
+                "type": "num",
+                "min": "100",
+                "max": "1000"
+            },
+            "SPLIT-chunk-overlap": {
+                "label": "Chunk Overlap",
+                "type": "num",
+                "min": "0",
+                "max": "100"
+            }
+        }
+    },
+    "COMBINE": {
+        "maps": {
+            "none": ["none"],
+            "single": ["none"],
+            "multi": ["single"]
+        },
+        "parameters": {
+
+        }
+    },
+    "LLM": {
+        "maps": {
+            "none": ["none"],
+            "single": ["single"],
+            "multi": ["multi"]
+        },
+        "parameters": {
+            "LLM-query" : {
+                "label": "Query for LLM",
+                "type": "textbox"
+            }
+        }
+    }
+}
+
+let blockTypes;
+fetch("/src/blocktypes.json").then(response => response.json()).then(json => blockTypes = json);
+let apiTypes;
+fetch("/src/apitypes.json").then(response => response.json()).then(json => apiTypes = json);
+
+const apiTypes_backup = {
+    "OpenAI": {
+        "parameters": {
+            "OpenAI-APIkey": {
+                "label": "API Key",
+                "type": "text"
+            }
+        }
+    },
+    "Oobabooga": {
+        "parameters": {
+            "Oobabooga-URL": {
+                "label": "API URL",
+                "type": "text"
+            }
+        }
+    }
+}
+
+// Create block type submenus
+createSubmenusByType(blockTypes, document.getElementById("new-block-type"));
+createSubmenusByType(apiTypes, document.getElementById("api-settings-type"));
+
+// Make sidebar expand buttons work
+var expands = document.getElementsByClassName("sidebar-submenu-expand-button");
+for (var i = 0; i < expands.length; i++) {
+    expands[i].addEventListener("click", function (e) {
+        e.preventDefault();
+        this.classList.toggle("sidebar-submenu-expand-button-active");
+        var content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+// Prevent enter from submitting forms
+var forms = document.getElementsByClassName("sidebar-submenu-expand");
+for (var form of forms) {
+    form.addEventListener('keypress', function (e) {
+        if (e.keyCode === 13) {
+            e.preventDefault();
+        }
+    });
+}
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const cy = cytoscape({
         container: document.getElementById('flow-diagram'),
@@ -152,264 +296,3 @@ document.addEventListener('DOMContentLoaded', function () {
         return blocks;
     }
 });
-
-const blockTypes = {
-    "INPUT": {
-        "maps": {
-            "none": ["single"]
-        },
-        "parameters": {}
-    },
-    "INPUT-FIXED": {
-        "maps": {
-            "none": ["single"]
-        },
-        "parameters": {
-            "INPUT-FIXED-text": {
-                "label": "Fixed Text Input",
-                "type": "textbox"
-            }
-        }
-    },
-    "OUTPUT": {
-        "maps": {
-            "none": ["none"],
-            "single": ["none"],
-            "multi": ["none"]
-        },
-        "parameters": {}
-    },
-    "COPY": {
-        "maps": {
-            "none": ["none"],
-            "single": ["multi"],
-            "multi": ["multi"]
-        },
-        "parameters": {
-            "COPY-num-copies": {
-                "label": "Number of Copies",
-                "type": "num",
-                "min": "2",
-                "max": "100"
-            }
-        }
-    },
-    "SPLIT": {
-        "maps": {
-            "none": ["none"],
-            "single": ["multi"],
-            "multi": ["multi"]
-        },
-        "parameters": {
-            "SPLIT-chunk-size": {
-                "label": "Chunk Size",
-                "type": "num",
-                "min": "100",
-                "max": "1000"
-            },
-            "SPLIT-chunk-overlap": {
-                "label": "Chunk Overlap",
-                "type": "num",
-                "min": "0",
-                "max": "100"
-            }
-        }
-    },
-    "COMBINE": {
-        "maps": {
-            "none": ["none"],
-            "single": ["none"],
-            "multi": ["single"]
-        },
-        "parameters": {
-
-        }
-    },
-    "LLM": {
-        "maps": {
-            "none": ["none"],
-            "single": ["single"],
-            "multi": ["multi"]
-        },
-        "parameters": {
-            "LLM-query" : {
-                "label": "Query for LLM",
-                "type": "textbox"
-            }
-        }
-    }
-}
-
-const apiTypes = {
-    "OpenAI": {
-        "parameters": {
-            "OpenAI-APIkey": {
-                "label": "API Key",
-                "type": "text"
-            }
-        }
-    },
-    "Oobabooga": {
-        "parameters": {
-            "Oobabooga-URL": {
-                "label": "API URL",
-                "type": "text"
-            }
-        }
-    }
-}
-
-// Append a immediately after e
-function insertAfter(a, e) {
-    if (e.nextSibling) {
-        e.parentNode.insertBefore(a, e.nextSibling);
-    } else {
-        e.parentNode.appendChild(a);
-    }
-}
-
-// Append a immediately before e
-function insertBefore(a, e) {
-    e.parentNode.insertBefore(a, e);
-}
-
-// Creates submenus that are only visisble according to the choice of selectElement, according to the config object.
-// config should consist of string:object pairs, where the string is an option to be added to selectElement, and the object
-// describes the submenu to be created. The object should have an entry "parameters" mapping to an object of string:object pairs,
-// where this object has an entry "label" and an entry "type". "type" should be in choice,text,num,file. "label" is what goes next to
-// the field in the submenu.
-function createSubmenusByType(config, selectElement) {
-    for (var type of Object.keys(config)) {
-        // Create option in selectElement
-        var typeOption = document.createElement("option");
-        typeOption.setAttribute("value", type);
-        typeOption.innerText = type;
-        selectElement.appendChild(typeOption);
-        // Create submenu
-        var typeSubmenu = document.createElement("div");
-        var typeSubmenuHeader = document.createElement("strong");
-        typeSubmenuHeader.innerText = type + " Parameters";
-        typeSubmenu.appendChild(typeSubmenuHeader);
-        typeSubmenu.appendChild(document.createElement("br"));
-        typeSubmenu.setAttribute("id", type + "-submenu");
-        typeSubmenu.setAttribute("name", type);
-        typeSubmenu.setAttribute("class", "sidebar-submenu " + selectElement.getAttribute("name") + "-submenu")
-        typeSubmenu.style.display = "none";
-        let params = config[type]["parameters"];
-        if (params.length === 0) {
-            insertAfter(document.createElement("br"), selectElement);
-        }
-        for (var param of Object.keys(params)) {
-            var paramLabel = document.createElement("label");
-            paramLabel.setAttribute("for", param);
-            paramLabel.innerText = params[param]["label"];
-            typeSubmenu.appendChild(paramLabel);
-            switch (params[param]["type"]) {
-                //Dropdown menu
-                case "choice":
-                    var paramChoice = document.createElement("select");
-                    paramChoice.setAttribute("id", param);
-                    paramChoice.setAttribute("name", param);
-                    for (var choice of params[param]["choices"]) {
-                        var paramChoiceOption = document.createElement("option");
-                        paramChoiceOption.setAttribute("value", choice);
-                        paramChoiceOption.innerText = choice;
-                        paramChoice.appendChild(paramChoiceOption);
-                    }
-                    typeSubmenu.appendChild(paramChoice);
-                    typeSubmenu.appendChild(document.createElement("br"));
-                    break;
-                //Textline
-                case "text":
-                    var paramText = document.createElement("input");
-                    paramText.setAttribute("id", param);
-                    paramText.setAttribute("name", param);
-                    paramText.setAttribute("type", "text");
-                    typeSubmenu.appendChild(paramText);
-                    typeSubmenu.appendChild(document.createElement("br"));
-                    break;
-                case "textbox":
-                    var paramTextbox = document.createElement("textarea");
-                    paramTextbox.setAttribute("id", param);
-                    paramTextbox.setAttribute("name", param);
-                    typeSubmenu.appendChild(paramTextbox);
-                    typeSubmenu.appendChild(document.createElement("br"));
-                    break;
-                //Numeric input
-                case "num":
-                    var paramNum = document.createElement("input");
-                    var paramNumDisplay = document.createElement("output");
-                    paramNum.setAttribute("id", param);
-                    paramNum.setAttribute("name", param);
-                    paramNum.setAttribute("type", "range");
-                    paramNum.setAttribute("min", params[param]["min"]);
-                    paramNum.setAttribute("max", params[param]["max"]);
-                    paramNumDisplay.textContent = paramNum.value;
-                    paramNum.addEventListener("input", function() {
-                        this.nextElementSibling.textContent = this.value;
-                    });
-                    typeSubmenu.appendChild(paramNum);
-                    typeSubmenu.appendChild(paramNumDisplay);
-                    typeSubmenu.appendChild(document.createElement("br"))
-                    break;
-                //File input
-                case "file":
-                    var paramFile = document.createElement("input");
-                    paramFile.setAttribute("id", param);
-                    paramFile.setAttribute("name", param);
-                    paramFile.setAttribute("type", "file");
-                    typeSubmenu.appendChild(paramFile);
-                    typeSubmenu.appendChild(document.createElement("br"));
-                    break;
-                default:
-                    break;
-            }
-        }
-        insertAfter(typeSubmenu, selectElement);
-    }
-
-    var firstType = Object.keys(config)[0];
-    if (!(Object.keys(config[firstType]["parameters"]).length === 0)) {
-        document.getElementById(firstType + "-submenu").style.display = "block";
-    }
-    selectElement.addEventListener("change", function () {
-        let selectedValue = selectElement.options[selectElement.selectedIndex].value;
-        for (var type of Object.keys(config)) {
-            if (selectedValue === type && !(Object.keys(config[type]["parameters"]).length === 0)) {
-                document.getElementById(type+"-submenu").style.display = "block";
-            } else {
-                document.getElementById(type+"-submenu").style.display = "none";
-            }
-        }
-    });
-}
-
-
-// Create block type submenus
-createSubmenusByType(blockTypes, document.getElementById("new-block-type"));
-createSubmenusByType(apiTypes, document.getElementById("api-settings-type"));
-
-// Make sidebar expand buttons work
-var expands = document.getElementsByClassName("sidebar-submenu-expand-button");
-for (var i = 0; i < expands.length; i++) {
-    expands[i].addEventListener("click", function (e) {
-        e.preventDefault();
-        this.classList.toggle("sidebar-submenu-expand-button-active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    });
-}
-
-// Prevent enter from submitting forms
-var forms = document.getElementsByClassName("sidebar-submenu-expand");
-for (var form of forms) {
-    form.addEventListener('keypress', function (e) {
-        if (e.keyCode === 13) {
-            e.preventDefault();
-        }
-    });
-}
