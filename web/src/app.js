b34301f0560e4de4fc26c72df67eb07449eb116f
Flurb <flurb18@gmail.com>
2023-09-12 00:47:23 -0400

    arrow scales on its own?

--- web/src/app.js
+++ web/src/app.js
@@ -33,8 +33,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 selector: 'edge.multi',
                 style: {
                     'width': 15,
-                    'line-style': 'dashed',
-                    'arrow-scale': 2
+                    'line-style': 'dashed'
                 }
             }
         ],
