65a8eccbc8060ea917d9d8d15d0be8adb5f65819
Flurb <flurb18@gmail.com>
2023-09-12 18:59:53 -0400

    correcct order

--- web/src/app.js
+++ web/src/app.js
@@ -25,142 +25,146 @@ for (var form of forms) {
     });
 }
 
-const blockTypesPromise = fetch("/src/blocktypes.json").then(response => response.json());
-const apiTypesPromise = fetch("/src/apitypes.json").then(response => response.json());
-const cytostylePromise = fetch("/styles/cytoscape-styles.json").then(response => response.json());
+document.addEventListener('DOMContentLoaded', function () {
+    const blockTypesPromise = fetch("/src/blocktypes.json").then(response => response.json());
+    const apiTypesPromise = fetch("/src/apitypes.json").then(response => response.json());
+    const cytostylePromise = fetch("/styles/cytoscape-styles.json").then(response => response.json());
 
-Promise.all([blockTypesPromise, apiTypesPromise, cytostylePromise]).then(
-    function (responses) {
-        let blockTypes = responses[0];
-        let apiTypes = responses[1];
-        let cytostyle = responses[2];
-        // Create block type submenus
-        createSubmenusByType(blockTypes, document.getElementById("new-block-type"));
-        createSubmenusByType(apiTypes, document.getElementById("api-settings-type"));
+    Promise.all([blockTypesPromise, apiTypesPromise, cytostylePromise]).then(
+        function (responses) {
+            let blockTypes = responses[0];
+            let apiTypes = responses[1];
+            let cytostyle = responses[2];
+            // Create block type submenus
+            createSubmenusByType(blockTypes, document.getElementById("new-block-type"));
+            createSubmenusByType(apiTypes, document.getElementById("api-settings-type"));
+            // Create the cytoscape
+            main(blockTypes, apiTypes, cytostyle);
+        }
+    );
+});
 
-        document.addEventListener('DOMContentLoaded', function () {
-            const cy = cytoscape({
-                container: document.getElementById('flow-diagram'),
-                zoom: 1,
-                minZoom: 0.5,
-                maxZoom: 2,
-                elements: [],
-                style: cytostyle,
-                layout: { name: 'grid' }
-            });
-        
-            var selectedNode = null;
-            cy.on('cxttap', function (event) {
-                if (event.target === cy) {
-                    if (selectedNode) {
+function main(blockTypes, apiTypes, cytostyle) {
+    // Run Cytoscape
+    const cy = cytoscape({
+        container: document.getElementById('flow-diagram'),
+        zoom: 1,
+        minZoom: 0.5,
+        maxZoom: 2,
+        elements: [],
+        style: cytostyle,
+        layout: { name: 'grid' }
+    });
+
+    var selectedNode = null;
+    cy.on('cxttap', function (event) {
+        if (event.target === cy) {
+            if (selectedNode) {
+                selectedNode.toggleClass("selected");
+                selectedNode = null;
+            }
+        } else {
+            if (event.target.isNode()) {
+                if (selectedNode) {
+                    if (!(selectedNode === event.target)) {
+                        var srcBlockType = selectedNode.data("block-type");
+                        var destBlockType = event.target.data("block-type");
+                        var srcOutputTypes = blockTypes[srcBlockType]["maps"][selectedNode.data("input-type")];
+                        if (srcOutputTypes.length === 1) {
+                            var srcOutputType = srcOutputTypes[0];
+                            var destAssignedInputType = event.target.data("input-type");
+                            var destAvailableInputTypes = Object.keys(blockTypes[destBlockType]["maps"]);
+                            if (!(srcOutputType === "none") && (
+                                (destAssignedInputType === srcOutputType) ||
+                                (destAssignedInputType === "none" && destAvailableInputTypes.includes(srcOutputType))
+                                )
+                            ) {
+                                var _classes = []
+                                if (srcOutputType === "multi") {
+                                    _classes.push("multi")
+                                }
+                                cy.add([{
+                                    "group": 'edges',
+                                    "data": {
+                                        "id": selectedNode.id() + event.target.id(),
+                                        "source": selectedNode.id(),
+                                        "target": event.target.id(),
+                                        "flow-type": srcOutputType
+                                    },
+                                    "classes": _classes
+                                }]);
+                                event.target.data("input-type", srcOutputType);
+                            }
+                        }
                         selectedNode.toggleClass("selected");
                         selectedNode = null;
                     }
                 } else {
-                    if (event.target.isNode()) {
-                        if (selectedNode) {
-                            if (!(selectedNode === event.target)) {
-                                var srcBlockType = selectedNode.data("block-type");
-                                var destBlockType = event.target.data("block-type");
-                                var srcOutputTypes = blockTypes[srcBlockType]["maps"][selectedNode.data("input-type")];
-                                if (srcOutputTypes.length === 1) {
-                                    var srcOutputType = srcOutputTypes[0];
-                                    var destAssignedInputType = event.target.data("input-type");
-                                    var destAvailableInputTypes = Object.keys(blockTypes[destBlockType]["maps"]);
-                                    if (!(srcOutputType === "none") && (
-                                        (destAssignedInputType === srcOutputType) ||
-                                        (destAssignedInputType === "none" && destAvailableInputTypes.includes(srcOutputType))
-                                        )
-                                    ) {
-                                        var _classes = []
-                                        if (srcOutputType === "multi") {
-                                            _classes.push("multi")
-                                        }
-                                        cy.add([{
-                                            "group": 'edges',
-                                            "data": {
-                                                "id": selectedNode.id() + event.target.id(),
-                                                "source": selectedNode.id(),
-                                                "target": event.target.id(),
-                                                "flow-type": srcOutputType
-                                            },
-                                            "classes": _classes
-                                        }]);
-                                        event.target.data("input-type", srcOutputType);
-                                    }
-                                }
-                                selectedNode.toggleClass("selected");
-                                selectedNode = null;
-                            }
-                        } else {
-                            selectedNode = event.target;
-                            selectedNode.toggleClass("selected");
-                        }
-                    }
+                    selectedNode = event.target;
+                    selectedNode.toggleClass("selected");
                 }
-            });
-        
-            var blockTypeIdNums = {}
-            for (var blockType of Object.keys(blockTypes)) {
-                blockTypeIdNums[blockType] = [0];
             }
-        
-            // Handle new block form submission
-            var newBlockForm = document.getElementById("new-block-form");
-            newBlockForm.addEventListener("submit", function (e) {
-                console.log("poop");
-                e.preventDefault();
-                var extent = cy.extent();
-                var blockType = newBlockForm.elements["new-block-type"].value;
-                var newId = Math.max(...blockTypeIdNums[blockType])+1;
-                var idString = blockType + newId;
-                var inputLabel = newBlockForm.elements["new-block-label"].value;
-                var label = inputLabel ? idString + "-" + inputLabel : idString;
-                var _data = {
-                    "id": idString,
-                    "label": label,
-                    "block-type": blockType,
-                    "input-type": "none",
-                    "parameters" : {}
-                };
-                blockTypeIdNums[blockType].push(newId);
-                for (param of Object.keys(blockTypes[blockType]["parameters"])) {
-                    _data["parameters"][param] = newBlockForm.elements[param].value;
-                }
-                var maxD = Math.floor(Math.min(extent.w, extent.h) / 6);
-                cy.add([{
-                    group: 'nodes',
-                    data: _data,
-                    position: {
-                        x: ((extent.x1 + extent.x2) / 2) + Math.floor(Math.random() * 2 * maxD) - maxD,
-                        y: ((extent.y1 + extent.y2) / 2) + Math.floor(Math.random() * 2 * maxD) - maxD
-                    }
-                }]);
-                if (blockType === "INPUT") {
-                    inputIdString = idString+"-input";
-                    var inputElement = document.createElement("textarea");
-                    inputElement.setAttribute("id", inputIdString);
-                    inputElement.setAttribute("name", inputIdString);
-                    var labelElement = document.createElement("label");
-                    labelElement.setAttribute("for", inputIdString);
-                    labelElement.innerText = label;
-                    var submitButton = document.getElementById("execute-form-submit");
-                    insertBefore(labelElement, submitButton);
-                    insertBefore(inputElement, submitButton);
-                    insertBefore(document.createElement("br"), submitButton);
-                }
-            });
-        
-            function getBlocksOfType(blockType) {
-                var blocks = [];
-                for (var id of blockTypeIdNums[blockType]) {
-                    if (!(id === 0)) {
-                        blocks.push(cy.nodes().getElementById(blockType + id));
-                    }
-                }
-                return blocks;
-            }
-        });
+        }
+    });
+
+    var blockTypeIdNums = {}
+    for (var blockType of Object.keys(blockTypes)) {
+        blockTypeIdNums[blockType] = [0];
     }
-);
+
+    // Handle new block form submission
+    var newBlockForm = document.getElementById("new-block-form");
+    newBlockForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        var extent = cy.extent();
+        var blockType = newBlockForm.elements["new-block-type"].value;
+        var newId = Math.max(...blockTypeIdNums[blockType])+1;
+        var idString = blockType + newId;
+        var inputLabel = newBlockForm.elements["new-block-label"].value;
+        var label = inputLabel ? idString + "-" + inputLabel : idString;
+        var _data = {
+            "id": idString,
+            "label": label,
+            "block-type": blockType,
+            "input-type": "none",
+            "parameters" : {}
+        };
+        blockTypeIdNums[blockType].push(newId);
+        for (param of Object.keys(blockTypes[blockType]["parameters"])) {
+            _data["parameters"][param] = newBlockForm.elements[param].value;
+        }
+        var maxD = Math.floor(Math.min(extent.w, extent.h) / 6);
+        cy.add([{
+            group: 'nodes',
+            data: _data,
+            position: {
+                x: ((extent.x1 + extent.x2) / 2) + Math.floor(Math.random() * 2 * maxD) - maxD,
+                y: ((extent.y1 + extent.y2) / 2) + Math.floor(Math.random() * 2 * maxD) - maxD
+            }
+        }]);
+        if (blockType === "INPUT") {
+            inputIdString = idString+"-input";
+            var inputElement = document.createElement("textarea");
+            inputElement.setAttribute("id", inputIdString);
+            inputElement.setAttribute("name", inputIdString);
+            var labelElement = document.createElement("label");
+            labelElement.setAttribute("for", inputIdString);
+            labelElement.innerText = label;
+            var submitButton = document.getElementById("execute-form-submit");
+            insertBefore(labelElement, submitButton);
+            insertBefore(inputElement, submitButton);
+            insertBefore(document.createElement("br"), submitButton);
+        }
+    });
+
+    function getBlocksOfType(blockType) {
+        var blocks = [];
+        for (var id of blockTypeIdNums[blockType]) {
+            if (!(id === 0)) {
+                blocks.push(cy.nodes().getElementById(blockType + id));
+            }
+        }
+        return blocks;
+    }
+}
 
