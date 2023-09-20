ee65223d05fd191c8e181ab9ba4b82002629b868
Flurb <flurb18@gmail.com>
2023-09-20 19:37:32 -0400

    lets try it

--- src/run.js
+++ src/run.js
@@ -3,54 +3,23 @@ import { resetBlock } from './utils.js';
 import { blockFuncs } from './blockfuncs.js';
 
 export function activateBlock(input, block, srcId, state) {
-    if (state.cancel) {
+    if (state.cancel || !state.running) {
         return new Promise((resolve, reject) => reject("Stopped"));
     }
-    return new Promise((resolve, reject) => {
-        if (!(block.data("waits-for").length == 0)) {
-            var waitIds = [...block.scratch("waiting-for")];
-            var queuedInputs = block.scratch("queued-inputs");
-            var idx = waitIds.indexOf(srcId);
-            if (idx > -1) {
-                block.addClass("waiting");
-                waitIds.splice(idx, 1);
-                queuedInputs[srcId] = input;
-                if (waitIds.length == 0) {
-                    block.removeClass("waiting")
-                    block.addClass("active");
-                    setTimeout(() => {
-                        executeBlock(queuedInputs, block, state).then(executeOutput => {
-                            resolve(executeOutput);
-                            resetBlock(block);
-                        }).catch(error => {
-                            reject(error);
-                            resetBlock(block);
-                        });
-                    }, 500);
-                } else {
-                    block.scratch("queued-inputs", queuedInputs);
-                    block.scratch("waiting-for", waitIds);
-                    resolve([{
-                        done: false
-                    }]);
-                }
-            } else {
-                console.log("Waiting block" + block.id() + " got extra input");
-            }
-        } else {
-            block.data("default-input-queue").push({
-                "input": input,
-                "resolve": resolve,
-                "reject": reject
+    return new Promise((resolve, reject) => {  
+        block.data("default-input-queue").push({
+            "input": input,
+            "from": srcId,
+            "resolve": resolve,
+            "reject": reject
+        });
+        if (!block.hasClass("running")) {
+            block.addClass("running");
+            executeBlockQueue(block, state).then((out) => {
+                block.removeClass("running");
+            }).catch((reason) => {
+                block.removeClass("running");
             });
-            if (!block.hasClass("running")) {
-                block.addClass("running");
-                executeBlockQueue(block, state).then((out) => {
-                    block.removeClass("running");
-                }).catch((reason) => {
-                    block.removeClass("running");
-                });
-            }
         }
     }).then((statuses) => {
         var activationPromises = [];
@@ -70,22 +39,61 @@ export function activateBlock(input, block, srcId, state) {
 }
 
 function executeBlockQueue(block, state) {
+    if (state.cancel) {
+        return new Promise((resolve, reject) => reject("Stopped"));
+    }
     return new Promise((resolve, reject) => {
         const queueItem = block.data("default-input-queue").shift();
-        block.addClass("active");
-        setTimeout(() => {
-            executeBlock(queueItem["input"], block, state).then(executeOutput => {
-                block.removeClass("active");
-                queueItem["resolve"](executeOutput);
+        if (!(block.data("waits-for").length == 0)) {
+            var waitIds = [...block.scratch("waiting-for")];
+            var queuedInputs = block.scratch("queued-inputs");
+            var idx = waitIds.indexOf(queueItem["from"]);
+            if (idx > -1) {
+                block.addClass("waiting");
+                waitIds.splice(idx, 1);
+                queuedInputs[queueItem["from"]] = queueItem["input"];
+                if (waitIds.length == 0) {
+                    block.removeClass("waiting")
+                    block.addClass("active");
+                    setTimeout(() => {
+                        executeBlock(queuedInputs, block, state).then(executeOutput => {
+                            queueItem["resolve"](executeOutput);
+                            resetBlock(block);
+                            resolve();
+                        }).catch(error => {
+                            queueItem["reject"](error);
+                            resetBlock(block);
+                            reject();
+                        });
+                    }, 500);
+                } else {
+                    block.scratch("queued-inputs", queuedInputs);
+                    block.scratch("waiting-for", waitIds);
+                    queueItem["resolve"]([{
+                        done: false
+                    }]);
+                    resolve();
+                }
+            } else {
+                block.data("default-input-queue").push(queueItem);
                 resolve();
-            }).catch(error => {
-                block.removeClass("active");
-                queueItem["reject"](error);
-                reject();
-            });
-        }, 500);
+            }
+        } else { 
+            block.addClass("active");
+            setTimeout(() => {
+                executeBlock(queueItem["input"], block, state).then(executeOutput => {
+                    block.removeClass("active");
+                    queueItem["resolve"](executeOutput);
+                    resolve();
+                }).catch(error => {
+                    block.removeClass("active");
+                    queueItem["reject"](error);
+                    reject();
+                });
+            }, 500);
+        }
     }).then((out) => {
-        if (block.data("default-input-queue").length == 0) {
+        if (block.data("default-input-queue").length == 0 || !state.running || state.cancel) {
             return Promise.resolve();
         } else {
             return executeBlockQueue(block, state);
@@ -94,7 +102,7 @@ function executeBlockQueue(block, state) {
 }
 
 function executeBlock(input, block, state) {
-    if (state.cancel) {
+    if (state.cancel || !state.running) {
         return new Promise((resolve, reject) => reject("Stopped"));
     }
     return new Promise((resolve, reject) => {
