7393a10c786a7fcb101732b6f54a77383cd1a102
Flurb <flurb18@gmail.com>
2023-09-22 07:29:13 -0400

    df

--- dist/config.js
+++ dist/config.js
@@ -4,7 +4,7 @@ var OPENAI_MODEL = "gpt-3.5-turbo";
 var ENABLE_OPENAI = false;
 // If using Oobabooga - put the url to your Oobabooga API here, with host and port, and set ENABLE_OOBABOOGA to true
 // If you are running default oobabooga with --api on the same machine as you are running this, it should be "http://localhost:5000".
-var OOBABOOGA_API_URL = "https://textapi.dserver.me";
+var OOBABOOGA_API_URL = "http://textgen-built:5000";
 var ENABLE_OOBABOOGA = true;
 // If using KoboldCPP - Put the url to your KoboldCPP API here, with host and port, and set ENABLE_KOBOLDCPP to true
 // IF you are running default KoboldCPP, it should be the same host and port that you use to connect to in browser.
